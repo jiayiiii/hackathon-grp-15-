09b1c2aaf3158f292bede3473eb9b18998f5eb3e
jiayi <yipjiayi11@gmail.com>
2024-11-12 15:29:53 +0800

    made the final thing!!

---
+++ hackathon (grp 15)/RomanTicTacToeView.swift
@@ -0,0 +1,18 @@
+//
+//  RomanTicTacToeView.swift
+//  hackathon (grp 15)
+//
+//  Created by jia yi on 12/11/24.
+//
+
+import SwiftUI
+
+struct RomanTicTacToeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RomanTicTacToeView()
+}
