9904e546c55ac9fa831ed839c621fcb3605ee787
Ooi Jing Kai <ooijk211@gmail.com>
2024-11-12 14:09:08 +0800

    homepage

--- hackathon (grp 15)/ContentView.swift
+++ hackathon (grp 15)/ContentView.swift
@@ -6,19 +6,40 @@ struct ContentView: View {
             ZStack {
                 Color.color
                     .ignoresSafeArea()
-                VStack {
-                    Text("tik tok toes")
-                        .font(.system(size:36))
+                VStack(spacing: 20) {
+                    Text("Tik Tok Toes")
+                        .font(.system(size: 45))
                         .fontWeight(.heavy)
+                        .padding(.bottom, 10)
+
+                    Text("Our team has developed a new Tic Tac Toe game different from the traditional Tic Tac Toe game!")
+                        .font(.system(size: 20))
+                        .foregroundColor(.black)
+                        .padding()
+
+                    Text("Unique Features:")
+                        .font(.headline)
+                        .foregroundColor(.black)
+                        .padding(.top)
+
+                    VStack() {
+                        Text("• Each player has three pieces (X, O), and after placing them, they can move them around.")
+                        Text("• 50/50 chance to get a perk, such as an extra turn or a skip turn.")
+                    }
+                    Spacer()
                     NavigationLink {
                         GameView()
                     } label: {
-                        Text("start")
+                        Text("Start")
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.black.opacity(0.1))
+                            .cornerRadius(10)
                     }
-                    .buttonStyle(.bordered)
-                    .foregroundColor(.black)
+                    
                 }
-        }
+                .padding()
+            }
         }
     }
 }
