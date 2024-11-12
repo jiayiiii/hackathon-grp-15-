6d0dd45cb2b7f96c9088a98b61b4c00f961dc48b
jiayi <yipjiayi11@gmail.com>
2024-11-12 13:06:54 +0800

    hjhjh

--- hackathon (grp 15)/ContentView.swift
+++ hackathon (grp 15)/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Abcasbdkahsdkdjad")
+            Text("okokoko")
         }
         .padding()
     }
